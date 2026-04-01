0cd632ba4790a6b07b97866a439e0f38b474e7c6
kazu0112 <kazuma0112@icloud.com>
2026-04-01 18:58:10 +0900

    演習1

---
+++ Iphone2/src/jp/co/diworks/action/Mp3Player.java
@@ -0,0 +1,13 @@
+package jp.co.diworks.action;
+
+public interface Mp3Player {
+	
+	public abstract void paly();
+	
+	public abstract void stop();
+	
+	public abstract void next();
+	
+	public abstract void back();
+
+}
